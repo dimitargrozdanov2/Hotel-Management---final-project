435eb8fcab3086d51eb001d83987955db637a700
tihomirtodorov <tihomir9todorov@gmail.com>
2019-05-21 17:30:13 +0300

    Added Role wrapper and User wrapper

---
+++ HotelManagement/HotelManagement.Web/Utilities/Wrappers/RoleManagerWrapper.cs
@@ -0,0 +1,73 @@
+﻿using HotelManagement.Services.Exceptions;
+using HotelManagement.Web.Utilities.Wrappers.Contracts;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelManagement.Web.Utilities.Wrappers
+{
+    public class RoleManagerWrapper : IRoleManagerWrapper
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public RoleManagerWrapper(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager ?? throw new ArgumentNullException(nameof(roleManager));
+        }
+
+        public async Task<IdentityResult> CreateRoleAsync(string name)
+        {
+            var role = await this.roleManager.FindByNameAsync(name);
+
+            if (role != null)
+            {
+                throw new EntityInvalidException("Role already exists!");
+            }
+
+            return await this.roleManager.CreateAsync(new IdentityRole(name));
+        }
+
+        public async Task<IdentityResult> DeleteRoleAsync(string name)
+        {
+            var role = await this.FindByNameAsync(name);
+
+            if (role == null)
+            {
+                throw new EntityInvalidException("Role not found!");
+            }
+
+            return await this.roleManager.DeleteAsync(role);
+        }
+
+        public async Task<IdentityRole> FindByNameAsync(string name)
+        {
+            var role = await this.roleManager.FindByNameAsync(name);
+
+            if (role == null)
+            {
+                throw new EntityInvalidException("Role not found!");
+            }
+
+            return role;
+        }
+
+        public IQueryable<IdentityRole> GetAllRoles()
+        {
+            var roles = this.roleManager.Roles;
+
+            return roles;
+        }
+
+        public async Task<IdentityResult> UpdateRoleAsync(IdentityRole roleModel)
+        {
+            if (roleModel.NormalizedName == "ADMIN")
+            {
+                throw new EntityInvalidException("You are not allowed to edit the Admin role!");
+            }
+
+            return await this.roleManager.UpdateAsync(roleModel);
+        }
+    }
+}
---
+++ HotelManagement/HotelManagement.Web/Utilities/Wrappers/UserManagerWrapper.cs
@@ -0,0 +1,33 @@
+﻿using HotelManagement.DataModels;
+using HotelManagement.Web.Utilities.Wrappers.Contracts;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Threading.Tasks;
+
+namespace HotelManagement.Web.Utilities.Wrappers
+{
+    public class UserManagerWrapper : IUserManagerWrapper
+    {
+        private readonly UserManager<User> userManager;
+
+        public UserManagerWrapper(UserManager<User> userManager)
+        {
+            this.userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
+        }
+
+        public async Task<User> FindByNameAsync(string username)
+        {
+            return await this.userManager.FindByNameAsync(username);
+        }
+
+        public async Task<IdentityResult> AddToRoleAsync(User user, string role)
+        {
+            return await this.userManager.AddToRoleAsync(user, role);
+        }
+
+        public async Task<IdentityResult> UpdateUserAsync(User user)
+        {
+            return await this.userManager.UpdateAsync(user);
+        }
+    }
+}
