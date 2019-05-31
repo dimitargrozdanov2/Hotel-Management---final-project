f8fbe7289c845f4122c22e6f7313e73ccf0b96a8
grozdeto <gros2@abv.bg>
2019-06-01 01:12:36 +0300

    Added Administrator menu with dropdowns

---
+++ HotelManagement/HotelManagement.Web/Areas/Administration/Controllers/AdminController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HotelManagement.Services.Contracts;
+using HotelManagement.Web.Utilities.Wrappers.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelManagement.Web.Areas.Administration.Controllers
+{
+    [Area("Administration")]
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        private readonly IUserManagerWrapper userManagerWrapper;
+        private readonly IUserService userService;
+
+        public AdminController(IUserManagerWrapper userManagerWrapper,
+            IUserService userService)
+        {
+            this.userManagerWrapper = userManagerWrapper;
+            this.userService = userService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
