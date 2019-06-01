8a03ab70d83c092c5b00c712aa035a722946ad10
grozdeto <gros2@abv.bg>
2019-06-01 12:48:57 +0300

    Added js for mouseenter and mouseleave on dropdown menu Administration, also added reference to site.js in the Layout

--- HotelManagement/HotelManagement.Web/wwwroot/js/site.js
+++ HotelManagement/HotelManagement.Web/wwwroot/js/site.js
@@ -1,1 +1,7 @@
 ﻿// Write your JavaScript code.
+$('.dropdown').on('mouseenter', () => {
+    $('.dropdown-menu').show();
+});
+$('.dropdown').on('mouseleave', () => {
+    $('.dropdown-menu').hide();
+});
