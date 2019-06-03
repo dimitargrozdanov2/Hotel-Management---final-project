c478737982cb70a12606b9cf6eec47092cbc095d
tihomirtodorov <tihomir9todorov@gmail.com>
2019-06-03 22:22:05 +0300

    Reworked _Layout, added new menu

--- HotelManagement/HotelManagement.Data/Migrations/20190603191329_Initial.Designer.cs
+++ HotelManagement/HotelManagement.Data/Migrations/20190603191329_Initial.Designer.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HotelManagement.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20190601150958_Initial")]
+    [Migration("20190603191329_Initial")]
     partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
@@ -517,6 +517,16 @@ namespace HotelManagement.Data.Migrations
                         {
                             UserId = "6404c00f-c0e6-4a92-ad71-43b24f5f0e97",
                             RoleId = "0e78767e-b4d2-456f-b029-5ad4c454589a"
+                        },
+                        new
+                        {
+                            UserId = "6404c00f-c0e6-4a92-ad71-43b24f5f0e97",
+                            RoleId = "ca8a9288-1bda-4320-8929-731d721be047"
+                        },
+                        new
+                        {
+                            UserId = "6404c00f-c0e6-4a92-ad71-43b24f5f0e97",
+                            RoleId = "2c9e83b9-1015-42b6-9df5-ad54b3d98224"
                         });
                 });
 
