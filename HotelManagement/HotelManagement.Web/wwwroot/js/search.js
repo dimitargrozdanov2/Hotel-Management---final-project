951c2059761bac3adcb18a583cc7ceb9b4c48b7a
tihomirtodorov <tihomir9todorov@gmail.com>
2019-06-13 09:12:51 +0300

    search func..added jquery msg when 0 notes are returned, old found notes are now being cleared upon new search

--- HotelManagement/HotelManagement.Web/wwwroot/js/search.js
+++ HotelManagement/HotelManagement.Web/wwwroot/js/search.js
@@ -1,6 +1,4 @@
 ﻿$("#target").keyup(function (event) {
-    //console.log("Handler for .keypress() called.");
-    //console.log(event.target.value);
     var userIdentity = event.target.dataset.id;
 
     if ($(this).val().length >= 2) {
@@ -12,11 +10,12 @@
         });
 
         request.done(function (data) {
-            console.log('success');
+            $('.pricing-table').empty();
+            if (data.length == 0) {
+                $('.pricing-table').prepend('<p id="notShownNotes" class="mt-5">I couldnt find any notes that matched the requirements, please try again!</p>')
+            }
             $.each(data, function (index) {
                 console.log(data[index]);
-                // alert("id= "+data[index].id+" name="+data[index].name);
-                //$('#myTable tbody').append("<tr class='child'><td>" + data[index].id + "</td><td>" + data[index].name + "</td></tr>");
                 var textToPrepend =
                     "<div class='pricing-option' id='idPlace'>" +
                     "<i class='material-icons'> mode_comment</i>" +
@@ -45,51 +44,6 @@
             console.log(data);
             console.log(data.responseText)
         })
-
-    //$.ajax({
-    //    url: "/Management/Management/GetNotesAsyncJson",
-    //    type: "POST",
-    //    data: { "data": event.target.value },
-    //    dataType: 'json',
-    //    success: function (data) {
-    //        console.log('success');
-    //        $.each(data, function (index) {
-    //            console.log(data[index].text);
-    //            console.log(data[index].id)
-    //            // alert("id= "+data[index].id+" name="+data[index].name);
-    //            //$('#myTable tbody').append("<tr class='child'><td>" + data[index].id + "</td><td>" + data[index].name + "</td></tr>");
-    //            var textToPrepend =
-    //                "<div class='pricing-option' id='idPlace'>" +
-    //                    "<i class='material-icons'> mode_comment</i>" +
-    //                    "<h1 class='note-title' id='titlePlace'>admin@admin.admin</h1>" +
-    //                    "<hr />" +
-    //                    "<p id='textPlace'>" + data[index].text + "</p>" +
-    //                    "<hr />" +
-    //                    "<p id='priorityPlace'>" + data[index].prioritytype + "</p>" +
-    //                    "<hr />" +
-    //                    "<div class='price'>" +
-    //                        "<div class='front'>" +
-    //                            "<span class='note-title'>@Model.Category?.Name </span>" +
-    //                        "</div>" +
-    //                        "<div class='back'>" +
-    //                            "<i class='material-icons' id='editNote' data-Id='@Model.Id'>edit</i>" +
-    //                            "<i class='material-icons' id='deleteNote' data-Id='@Model.Id'>remove_circle</i>" +
-    //                            "<i class='material-icons' id='archiveNote'>archive</i>" +
-    //                        "</div>" +
-    //                    "</div>" +
-    //                "</div>";
-
-    //            $('.pricing-table').prepend(textToPrepend)
-    //        });
-
-    //    },
-    //    //failure: function (response) {
-    //    //    alert(response.responseText);
-    //    //},
-    //    error: function (response) {
-    //        console.log(response.responseText);
-    //    }
-    //});
     }
 });
 
