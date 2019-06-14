7b7d4c07b0087e0e490108957a921a99ab1b0f1e
grozdeto <gros2@abv.bg>
2019-06-14 20:58:57 +0300

    fixed diamond logo path as the image path broke

--- HotelManagement/HotelManagement.Data/Migrations/20190614175553_Initial.Designer.cs
+++ HotelManagement/HotelManagement.Data/Migrations/20190614175553_Initial.Designer.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HotelManagement.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20190614161430_Initial")]
+    [Migration("20190614175553_Initial")]
     partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
